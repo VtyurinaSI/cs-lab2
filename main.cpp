baae27d48e98ef2a464a416278e719b0fa285656
Bob (VtyurinaSI) <svetlana0vtyurina@gmail.com>
2019-03-19 00:43:40 +0300

    code: боб с минимумом

--- main.cpp
+++ main.cpp
@@ -11,8 +11,18 @@ int main()
          << "A - B = " << a - b << '\n'
          << "A * B = " << a * b << '\n'
          << "A / B = " << a / b << '\n';
+
     int max;
-    if ( a < b ) max = b;
-    else max = a;
+    if ( a < b )
+        max = b;
+    else
+        max = a;
+    cout << max;
+    int min;
+    if ( a < b )
+        min = a;
+    else
+        min = b;
+    cout << max;
     return 0;
 }
